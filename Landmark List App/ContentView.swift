cfdc908bb79177110136d4724608cb8839903aaf
jisung <jisung.smart@gmail.com>
2020-05-26 13:29:54 +0900

    Initial Commit

---
+++ Landmark List App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmark List App
+//
+//  Created by The book Air on 2020/05/26.
+//  Copyright © 2020 The book Air. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
